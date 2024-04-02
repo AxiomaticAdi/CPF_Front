e9f919b979f3d1f73fa80dfe23523bd095b9172f
Adi <140542996+AxiomaticAdi@users.noreply.github.com>
2024-04-02 13:03:16 -0700

    remove unneeded console logs

--- src/services/fetchClassesService.ts
+++ src/services/fetchClassesService.ts
@@ -3,15 +3,12 @@ import { collection, getDocs } from "firebase/firestore";
 import { CookingClass } from "../types";
 
 export async function fetchCookingClasses(): Promise<CookingClass[]> {
-	console.log("Fetching cooking classes...");
+	console.log("Fetching data...");
 	const classesCollection = collection(db, "Classes");
-	console.log("Line 1");
 
 	try {
 		const snapshot = await getDocs(classesCollection);
-		console.log("Line 2");
 		const classes: CookingClass[] = [];
-		console.log("Line 3");
 
 		snapshot.forEach((doc) => {
 			const data = doc.data();
