c7593a530ba3a1b2a6cd938f1772efc660a91791
Adi <140542996+AxiomaticAdi@users.noreply.github.com>
2024-04-14 11:12:19 -0700

    Footer formatting on phone

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,26 +2,28 @@ import { Link } from "react-router-dom";
 
 export default function Footer() {
 	return (
-		<footer className="w-full pb-4 pt-24 flex justify-between text-sm text-gray-400 items-center">
-			<div>
+		<footer className="w-full pb-4 pt-24 flex justify-between text-xs sm:text-sm text-gray-400 items-center">
+			<div className="text-left">
 				<p>Copyright © 2024. All rights reserved.</p>
 			</div>
-			<div className="flex flex-wrap gap-2 items-center">
+			<div className="flex justify-end flex-wrap gap-2 items-center text-right">
 				<p>Connect with us!</p>
-				<Link to={"https://www.facebook.com/profile.php?id=100094433604365"}>
-					<img
-						src="https://cdn.svgporn.com/logos/facebook.svg"
-						alt="Facebook logo"
-						className="h-6 w-6 filter grayscale"
-					/>
-				</Link>
-				<Link to={"https://www.facebook.com/profile.php?id=100094433604365"}>
-					<img
-						src="https://cdn.svgporn.com/logos/instagram-icon.svg"
-						alt="Instagram logo"
-						className="h-6 w-6 filter grayscale"
-					/>
-				</Link>
+				<div className="flex gap-2">
+					<Link to={"https://www.facebook.com/profile.php?id=100094433604365"}>
+						<img
+							src="https://cdn.svgporn.com/logos/facebook.svg"
+							alt="Facebook logo"
+							className="h-6 w-6 filter grayscale"
+						/>
+					</Link>
+					<Link to={"https://www.facebook.com/profile.php?id=100094433604365"}>
+						<img
+							src="https://cdn.svgporn.com/logos/instagram-icon.svg"
+							alt="Instagram logo"
+							className="h-6 w-6 filter grayscale"
+						/>
+					</Link>
+				</div>
 			</div>
 		</footer>
 	);
