b1c1756b3f52f1d948d1efb78a276c68026005d4
Adi <140542996+AxiomaticAdi@users.noreply.github.com>
2024-04-14 19:59:29 -0700

    Update classes context to subscription

--- src/contexts/ClassesProvider.tsx
+++ src/contexts/ClassesProvider.tsx
@@ -1,7 +1,8 @@
 import React, { ReactNode, useState, useEffect } from "react";
 import ClassesContext from "./ClassesContext";
 import { CookingClass } from "../types";
-import { fetchCookingClasses } from "../services/fetchClassesService";
+import { collection, onSnapshot, query } from "firebase/firestore";
+import db from "../config/firebase-config";
 
 interface ClassesProviderProps {
 	children: ReactNode;
@@ -13,10 +14,35 @@ const ClassesProvider: React.FC<ClassesProviderProps> = ({ children }) => {
 
 	useEffect(() => {
 		setIsLoading(true);
-		fetchCookingClasses().then((classes) => {
-			setCookingClasses(classes);
-			setIsLoading(false);
-		});
+		const q = query(collection(db, "Classes"));
+		const unsubscribe = onSnapshot(
+			q,
+			(querySnapshot) => {
+				const classes: CookingClass[] = querySnapshot.docs.map((doc) => {
+					const data = doc.data();
+					return {
+						id: doc.id,
+						name: data.Name,
+						description: data.Description,
+						imageUrl: data.ImageUrl,
+						startTime: data.StartDateTime.toDate(), // Convert Firestore timestamp to JavaScript Date
+						endTime: data.EndDateTime.toDate(), // Convert Firestore timestamp to JavaScript Date
+						capacity: data.Capacity,
+						sold: data.Sold,
+						price: data.Price,
+					} as CookingClass;
+				});
+				setCookingClasses(classes);
+				setIsLoading(false);
+			},
+			(error) => {
+				console.error("Error fetching cooking classes:", error);
+				setIsLoading(false); // Optionally handle error state
+			}
+		);
+
+		// Cleanup the listener on component unmount
+		return () => unsubscribe();
 	}, []);
 
 	return (
