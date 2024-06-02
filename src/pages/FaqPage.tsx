191a0b429a57ace9506430be2c34b8ac708dabea
AxiomaticAdi <arudra@bu.edu>
2024-06-01 18:15:56 -0700

    Updated FAQ

--- src/pages/FaqPage.tsx
+++ src/pages/FaqPage.tsx
@@ -7,13 +7,10 @@ export default function FaqPage() {
 
 			<div className="flex flex-col gap-6 text-center items-center">
 				<div>
-					<h2 className="text-xl font-semibold">
-						Will there be alcoholic drinks served during my event?
-					</h2>
+					<h2 className="text-xl font-semibold">Are tickets refundable?</h2>
 					<p className="max-w-xl">
-						We offer a rotating selection of wines by the glass and bottle, as
-						well as craft beers. Your first drink is on us! Additional beverages
-						can be purchased during your event.
+						Tickets are non-refundable. However, if you can’t make the dinner,
+						you are welcome to give or resell your tickets to someone else.
 					</p>
 				</div>
 			</div>
