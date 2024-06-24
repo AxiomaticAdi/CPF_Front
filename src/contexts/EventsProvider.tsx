b7f778f0a6c7a9e59fae7e756b26bbea5b278a2a
AxiomaticAdi <arudra@bu.edu>
2024-06-24 14:23:08 -0700

    refactor: only fetch events in future, and sort

--- src/contexts/EventsProvider.tsx
+++ src/contexts/EventsProvider.tsx
@@ -1,7 +1,13 @@
 import React, { ReactNode, useState, useEffect } from "react";
 import EventsContext from "./EventsContext";
 import { Event } from "../types";
-import { collection, onSnapshot, query } from "firebase/firestore";
+import {
+	collection,
+	onSnapshot,
+	query,
+	where,
+	orderBy,
+} from "firebase/firestore";
 import db from "../config/firebase-config";
 
 interface EventsProviderProps {
@@ -14,47 +20,47 @@ const EventsProvider: React.FC<EventsProviderProps> = ({ children }) => {
 
 	useEffect(() => {
 		setIsLoading(true);
-		const q = query(collection(db, "Events"));
+		const now = new Date();
+
+		// Only fetch future events, and order by time
+		const q = query(
+			collection(db, "Events"),
+			where("EndDateTime", ">", now),
+			orderBy("EndDateTime")
+		);
+
 		const unsubscribe = onSnapshot(
 			q,
 			(querySnapshot) => {
-				let events: Event[] = querySnapshot.docs.map((doc) => {
+				const events: Event[] = querySnapshot.docs.map((doc) => {
 					const data = doc.data();
 					return {
 						id: doc.id,
 						name: data.Name,
 						description: data.Description,
 						imageUrl: data.ImageUrl,
-						startTime: data.StartDateTime.toDate(), // Convert Firestore timestamp to JavaScript Date
-						endTime: data.EndDateTime.toDate(), // Convert Firestore timestamp to JavaScript Date
+						startTime: data.StartDateTime.toDate(),
+						endTime: data.EndDateTime.toDate(),
 						capacity: data.Capacity,
 						sold: data.Sold,
 						price: data.Price,
 					} as Event;
 				});
 
-				// Filter out events that have already passed
-				events = events.filter((event) => {
-					const now = new Date();
-					return now < event.endTime;
-				});
-
-				// Assign filtered events to state
 				setEvents(events);
 				setIsLoading(false);
 			},
 			(error) => {
 				console.error("Error fetching events:", error);
-				setIsLoading(false); // Optionally handle error state
+				setIsLoading(false);
 			}
 		);
 
-		// Cleanup the listener on component unmount
 		return () => unsubscribe();
 	}, []);
 
 	return (
-		<EventsContext.Provider value={{ events: events, isLoading }}>
+		<EventsContext.Provider value={{ events, isLoading }}>
 			{children}
 		</EventsContext.Provider>
 	);
