79ec3c4c8791e9c64abfe0bd46629cf243190cea
AxiomaticAdi <arudra@bu.edu>
2024-12-06 08:42:24 -0800

    feat: format price in badge component

--- src/components/Badges/BadgePrice.tsx
+++ src/components/Badges/BadgePrice.tsx
@@ -3,9 +3,11 @@ interface BadgePriceProps {
 }
 
 export default function BadgePrice({ price }: BadgePriceProps) {
+	const formattedPrice = Number.isInteger(price) ? price : price.toFixed(2);
+
 	return (
 		<div className="absolute top-4 right-4 bg-black text-white font-light p-2 rounded-md text-sm z-10 opacity-80">
-			<div className="text-xl opacity-100">${price}</div>
+			<div className="text-xl opacity-100">${formattedPrice}</div>
 		</div>
 	);
 }
